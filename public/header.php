bb99d520f92fb235bedfdf259a1104bad789a972
ori.av-sapir <oavsapir@gmail.com>
2023-05-11 09:55:34 +0300

    formmating header file

--- public/header.php
+++ public/header.php
@@ -17,7 +17,8 @@ if (isset($_SESSION['userId']) and $_SESSION['userId'] ==  1) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Connand and Control</title>
@@ -27,17 +28,17 @@ if (isset($_SESSION['userId']) and $_SESSION['userId'] ==  1) {
 
 <body class="bg-light">
     <div class="text-right">
-        <a href="dashboard" class="text-dark text-decoration-none"> Dashboard |</a>
-        <a href="profile" class="text-dark text-decoration-none"> Profile |</a>
-        <a href="error_log" class="text-dark text-decoration-none">Server error logs |</a>
-        <a href="log" class="text-dark text-decoration-none"> Command and response logs | </a>
-        <form action="" method="post" class="float-end">
-            <button name="logoff" class="bg-light border-0">Logout</button>
+        <a href="dashboard" class="text-dark text-decoration-none">Dashboard</a> |
+        <a href="profile" class="text-dark text-decoration-none">Profile</a> |
+        <a href="error_log" class="text-dark text-decoration-none">Server Error Logs</a> |
+        <a href="log" class="text-dark text-decoration-none">Command and Response Logs</a> |
+        <form action="" method="post" class="d-inline-block">
+            <button name="logoff" class="btn btn-light border-0">Logout</button>
         </form>
-        <div class="p-0 m-0 bg-white" style="min-height:10px;"></div>
     </div>
+    <div class="bg-white" style="height: 10px;"></div>
 
-<?php 
+    <?php 
 }else{
     http_response_code(404);
     die();
